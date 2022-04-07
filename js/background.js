e9cb80cd087f5f45804506c9c857788adc4403a4
Kms9306 <ms930610@naver.com>
2022-04-07 09:27:30 +0900

    2022-04-07

--- js/background.js
+++ js/background.js
@@ -3,4 +3,4 @@ const Container = document.querySelector(".container");
 const img = ["01.jpg","02.jpg","03.png"];
 const indexNandom = img[Math.floor(Math.random()*img.length)]
 Container.style.background = `url(img/${indexNandom}) no-repeat center center`;
-Container.style.backgroundSize = "cover"
+Container.style.backgroundSize = "cover";
