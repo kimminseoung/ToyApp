a6b39c9b947be9489418f598ad6eb7da09dc5184
Kms9306 <ms930610@naver.com>
2022-04-07 11:34:16 +0900

    2022-04-07

--- js/weather.js
+++ js/weather.js
@@ -2,6 +2,17 @@
 // 키 7dc7576b96fd893f52e7e5bf7f883448
 
 const MY_API_KEY = "7dc7576b96fd893f52e7e5bf7f883448";
+let weatherIcon = {
+    '01' : 'fa-solid fa-sun',
+    '02' : 'fa-duotone fa-sun-cloud',
+    '03' : 'fa-duotone fa-clouds',
+    '04' : 'fa-duotone fa-cloud-meatball',
+    '09' : 'fa-duotone fa-cloud-sun-rain',
+    '10' : 'fa-cloud-showers-heavy',
+    '11' : 'fa-duotone fa-poo-storm',
+    '13' : 'fa-duotone fa-snowflake',
+    '50' : 'fa-duotone fa-smog'
+}
 
 const weatherNav =  navigator.geolocation.watchPosition(liveGeo,liveGeoErr)
 const weatherText = document.getElementById("weather");
@@ -10,12 +21,14 @@ function liveGeo(position){
     const long = position.coords.longitude;
     fetch(`https://api.openweathermap.org/data/2.5/weather?&lang=kr&lat=${lat}&lon=${long}&appid=${MY_API_KEY}&units=metric`).then(res=>res.json()).then(data=>{
             const city = `${data.name}`;
+
             const weather  = `${data.weather[0].description}`;
             const WTemp = `${data.main.temp}`
+            const wIcon = (data.weather[0].icon).substr(0,2);
             weatherText.innerHTML =
             `
                 <div class="weathers">${city}</div>
-                <div class="weathers">${weather}</div>
+                <div class="weathers"><i class="${weatherIcon[wIcon]}"></i> ${weather}</div>
                 <div class="weathers">${Math.floor(WTemp)}℃</div>
             `;
         }
